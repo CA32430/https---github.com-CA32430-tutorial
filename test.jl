3b7ef839f091849eeac4fab662d8382070636d49
CA32430 <casey.macgibbon@ll.mit.edu>
2023-06-27 11:01:17 -0400

    first

--- test.jl
+++ test.jl
@@ -1,2 +1,12 @@
-println("hello world")
+import Pkg; Pkg.add("DataFrames")
+using DataFrames
+import Pkg; Pkg.add("SimpleGraphs")
+using SimpleGraphs
+
+
+function df_fun(df)
+    df = DataFrame(a = [1,2,3,4],b = [3,4,5,6])
+    df[!, "row_num"]= rownumber.(eachrow(df))
+end
+
 
