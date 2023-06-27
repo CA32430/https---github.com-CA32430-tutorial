58778d6878d155bbd2ad58cfb7460c1e9c8c1b02
CA32430 <casey.macgibbon@ll.mit.edu>
2023-06-27 11:03:57 -0400

    hello world

--- test.jl
+++ test.jl
@@ -1,12 +1,3 @@
-import Pkg; Pkg.add("DataFrames")
-using DataFrames
-import Pkg; Pkg.add("SimpleGraphs")
-using SimpleGraphs
-
-
-function df_fun(df)
-    df = DataFrame(a = [1,2,3,4],b = [3,4,5,6])
-    df[!, "row_num"]= rownumber.(eachrow(df))
-end
+println("Hello World!")
 
 
